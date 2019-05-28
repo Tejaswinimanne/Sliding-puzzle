5c38653957de03dea714d37186e5710b98da748b
Tejaswinimanne <tejumanne1999@gmail.com>
2019-05-28 20:46:30 +0530

    second commit

--- game.js
+++ game.js
@@ -5,46 +5,44 @@ function swap(cell1,cell2) {
 }
 
 function shuffle() {
-//Use nested loops to access each cell of the 3x3 grid
-for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
-   for (var column=1;column<=3;column++) { //For each column in this row
-  
-    var row2=Math.floor(Math.random()*3 + 1); //Pick a random row from 1 to 3
-    var column2=Math.floor(Math.random()*3 + 1); //Pick a random column from 1 to 3
-     
-    swap("cell"+row+column,"cell"+row2+column2); //Swap the look & feel of both cells
+  var score=0;
+  document.getElementById('score').innerHTML = "Score: "+ score;
+for(var row=1;row<=3;row++) { 
+   for(var column=1;column<=3;column++) {
+      var row2=Math.floor(Math.random()*3 + 1); 
+      var column2=Math.floor(Math.random()*3 + 1); 
+      swap("cell"+row+column,"cell"+row2+column2); 
+    } 
   } 
-} 
 }
-
 function clickTile(row,column) {
   var cell = document.getElementById("cell"+row+column);
   var tile = cell.className;
-  if (tile!="tile9") { 
-       //Checking if white tile on the right
-       if (column<3) {
-         if ( document.getElementById("cell"+row+(column+1)).className=="tile9") {
+  if (tile!="tile9") {
+       if(column<3) {
+         if(document.getElementById("cell"+row+(column+1)).className=="tile9") {
+          document.getElementById('score').innerHTML = "Score: " + score++;
            swap("cell"+row+column,"cell"+row+(column+1));
            return;
          }
        }
-       //Checking if white tile on the left
        if (column>1) {
          if ( document.getElementById("cell"+row+(column-1)).className=="tile9") {
+          document.getElementById('score').innerHTML = "Score: " + score++;
            swap("cell"+row+column,"cell"+row+(column-1));
            return;
          }
        }
-         //Checking if white tile is above
        if (row>1) {
          if ( document.getElementById("cell"+(row-1)+column).className=="tile9") {
+          document.getElementById('score').innerHTML = "Score: " + score++;
            swap("cell"+row+column,"cell"+(row-1)+column);
            return;
          }
        }
-       //Checking if white tile is below
        if (row<3) {
          if ( document.getElementById("cell"+(row+1)+column).className=="tile9") {
+          document.getElementById('score').innerHTML = "Score: " + score++;
            swap("cell"+row+column,"cell"+(row+1)+column);
            return;
          }
